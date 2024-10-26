6b084a9588af52397e6a82595f21303322ebe828
Roshan Tiwaree <roshan0x01@gmail.com>
2024-10-26 11:56:17 +0545

    chore(strings): added more strings for complexity password.

--- bash/pass-gen.sh
+++ bash/pass-gen.sh
@@ -1,7 +1,7 @@
-
 #!/bin/bash
 # Author: @codedsprit <roshan0x01@gmail.com>
-#
-# Yes, password generator 
+# Password generator with additional characters
+# Set the length for the password, default to 25 if not provided
+
 length=${1:-25}
-< /dev/urandom tr -dc 'A-Za-z0-9_@#$%^&*' | head -c "$length"; echo
+< /dev/urandom tr -dc 'A-Za-z0-9_!@#$%^&*(){}[];:,.<>?123456789' | head -c "$length"; echo
