3fa3059a595d565bfc14a518df280b6e824a3940
Roshan Tiwaree <roshan0x01@gmail.com>
2024-10-27 00:20:11 +0545

    chore(script): minor update to ``auto-filr-download``

--- bash/auto-file-download.sh
+++ bash/auto-file-download.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+# A simple bash script to download image files from given url target.
 
 # Location of the URL list
-URL_LIST="/path/to/url-list.txt"
-
+URL_LIST="/home/sprit/hello/url.txt"
 # Directory where files will be downloaded
-DOWNLOAD_DIR="/path/to/download"
-
+DOWNLOAD_DIR="/home/sprit/hello"
 # Create the download directory if it doesn't exist
 mkdir -p "$DOWNLOAD_DIR"
-
 # Read each URL from the file and download it
 while IFS= read -r URL; do
-  if [[ ! -z "$URL" ]]; then
-    echo "Downloading $URL..."
-    wget -P "$DOWNLOAD_DIR" "$URL"
+  URL=$(echo "$URL" | xargs)
+
+  if [ -n "$URL" ]; then
+    FILENAME=$(basename "$URL")    
+# skipping dublicate file to download.
+    if [ -f "$DOWNLOAD_DIR/$FILENAME" ]; then
+      echo "File '$FILENAME' already exists. Skipping download."
+    else
+      echo "Downloading $URL..."
+      if wget -P "$DOWNLOAD_DIR" "$URL"; then
+        echo "Successfully downloaded '$FILENAME'."
+      else
+        echo "Failed to download '$URL'."
+      fi
+    fi
   fi
 done < "$URL_LIST"
 
-echo "All files downloaded to $DOWNLOAD_DIR."
+echo "All downloads processed. Files saved to $DOWNLOAD_DIR."
